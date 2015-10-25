02922c2c72002eec8fb42b90f3bd6b1d3ed0c260
Kenil Bhatt <kpbhatt@asu.edu>
2015-10-25 15:30:47 -0700

    added bankbalance.java

---
+++ UniversalBank/src/main/java/edu/asu/ss2015/group4/model/BankBalance.java
@@ -0,0 +1,24 @@
+package edu.asu.ss2015.group4.model;
+
+public class BankBalance {
+
+	private double checking;
+	private double saving;
+
+	public double getCheckingAccount() {
+		return checking;
+	}
+
+	public double getSavingAccount() {
+		return saving;
+	}
+
+	public void setCheckingAccount(double checking) {
+		this.checking = checking;
+	}
+
+	public void setSavingsAccount(double saving) {
+		this.saving = saving;
+	}
+
+}
